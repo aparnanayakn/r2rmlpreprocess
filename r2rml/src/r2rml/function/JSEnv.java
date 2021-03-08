23829e60ed3d7b1fcee647ccd13b19856c27f560
aparnanayakn <naparnanayak@gmail.com>
2021-03-08 09:56:16 +0000

    pending:space in column header

---
+++ r2rml/src/r2rml/function/JSEnv.java
@@ -0,0 +1,164 @@
+package r2rml.function;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.log4j.Logger;
+
+import r2rml.engine.RRF;
+
+/**
+ * JSEnv Class.
+ * 
+ * @author Christophe Debruyne
+ * @version 0.1
+ *
+ */
+public class JSEnv {
+
+	private static Logger logger = Logger.getLogger(JSEnv.class.getName());
+	
+	private static ScriptEngineManager manager = new ScriptEngineManager();
+	private static ScriptEngine engine = manager.getEngineByName("javascript");
+	
+	private static Map<Resource, String> nameMap = new HashMap<Resource, String>();
+
+	private static Model m = ModelFactory.createDefaultModel();
+	/**
+	 * Invoking a function with an array of parameters.
+	 * 
+	 * @param functionName
+	 * @param parameters
+	 * @return
+	 * @throws NoSuchMethodException
+	 * @throws ScriptException
+	 */
+	public static Literal invoke(String functionName, Object... parameters) 
+			throws NoSuchMethodException, ScriptException {
+		Invocable invokeEngine = (Invocable) engine;
+		String attachdt = "attachdt";
+		String attachlg = "attachlg";
+
+		if(functionName.equals(attachdt)) {
+			String dt=(String) parameters[1];
+			String s=(String) parameters[0];
+			Literal l = m.createTypedLiteral(s,dt);
+			//logger.info(m.createTypedLiteral(parameters[0],(RDFDatatype) parameters[1]));		
+		
+//			System.out.print(l);
+			return l; 
+			
+		} 
+		else if(functionName.equals(attachlg)) {
+			String dt=(String) parameters[1];
+			String s=(String) parameters[0];
+			Literal l = m.createLiteral(s,dt);
+			//logger.info(m.createTypedLiteral(parameters[0],(RDFDatatype) parameters[1]));		
+		
+			return l; 
+			
+		} 
+	
+		else { 
+			Object o = invokeEngine.invokeFunction(functionName, parameters);
+		//	logger.info(o.toString());
+			return (Literal)o;
+			
+		} 
+	}
+
+	/**
+	 * Loading JavaScript code.
+	 * 
+	 * @param code
+	 * @throws ScriptException
+	 */
+	public static void loadCode(String code) throws ScriptException {
+		engine.eval(code);
+	}
+
+	/**
+	 * A utility function for setting up a new engine and environment.
+	 * 
+	 */
+	public static void reset() {
+		manager = new ScriptEngineManager();
+		manager.getEngineByName("javascript");
+		nameMap.clear();
+	}
+
+	public static String registerFunction(RDFNode node) {
+		if(!node.isResource()) {
+			logger.error("Function valued TermMap's function must be a resource.");
+			logger.error(node);
+			return null;
+		}
+		
+		Resource f = node.asResource();
+		
+		List<Statement> names = f.listProperties(RRF.functionName).toList();
+		if(names.size() != 1) {
+			logger.error("Functions must have exactly one rrf:functionName.");
+			logger.error(f);
+			return null;
+		}
+		
+		if(!names.get(0).getObject().isLiteral()) {
+			logger.error("rrf:functionName must be a literal.");
+			logger.error(f);
+			return null;
+		}
+		
+		String name = names.get(0).getObject().asLiteral().getLexicalForm();
+		
+		if(!nameMap.containsKey(f) && nameMap.values().contains(name)) {
+			logger.error("No two functions can have the same rrf:functionName.");
+			logger.error(f);
+			return null;
+		}
+		
+		nameMap.put(f, name);
+		logger.info("Registered function " + name);
+		
+		List<Statement> bodies = f.listProperties(RRF.functionBody).toList();
+		if(bodies.size() != 1) {
+			logger.error("Functions must have exactly one rrf:functionBody.");
+			logger.error(f);
+			return null;
+		}
+		
+		if(!bodies.get(0).getObject().isLiteral()) {
+			logger.error("rrf:functionBody must be a literal.");
+			logger.error(f);
+			return null;
+		}
+		
+		String body = bodies.get(0).getObject().asLiteral().getLexicalForm();
+		
+		try {
+			loadCode(body);
+			logger.info("Loaded function " + name);
+			
+		} catch (ScriptException e) {
+			logger.error("rrf:functionBody contains issues.");
+			logger.error(e.getMessage());
+			logger.error(f);
+			return null;
+		}
+		
+		return name;
+	}
+
+}
