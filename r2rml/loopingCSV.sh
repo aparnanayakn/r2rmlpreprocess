09acb363bf666a6f643e41a5b22ec218340cde31
aparnanayakn <naparnanayak@gmail.com>
2021-03-09 08:55:35 +0000

    updated directory structure

--- r2rml/loopingCSV.sh
+++ r2rml/loopingCSV.sh
@@ -2,5 +2,5 @@
 #
 while IFS=, read -r one two three four; do 
     echo "The 1st field is $one, the 2nd $two and the 3rd $three"; 
-    java -jar ./target/r2rml.jar ./testcases/$one ./testcases/$two ./testcases/outputs/$three $four
+    java -jar ./target/r2rml.jar ./testcases/input/$one ./testcases/mapping/$two ./testcases/outputs/$three $four
   done < testcases.csv
